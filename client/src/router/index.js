868bb533222a944ef2fdf3010ed00a890a5ff711
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-06-15 15:21:48 +0500

    add routes

---
+++ client/src/router/index.js
@@ -0,0 +1,8 @@
+import CreateTask from "../CreateTask";
+import Home from "../Home";
+
+
+export const routes = [
+    { path: '/home', component: Home },
+    { path: '/upload', component: CreateTask },
+]
