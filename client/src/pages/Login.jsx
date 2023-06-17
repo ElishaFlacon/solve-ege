55e4ab1c2269839686820b3ed7ccaacdcfbc4aa3
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-06-17 10:38:30 +0500

    add login page

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import LoginForm from '../components/LoginForm';
+
+
+function Login() {
+
+
+    return (
+        <div>
+            <LoginForm />
+        </div>
+    );
+}
+
+
+export default Login;
