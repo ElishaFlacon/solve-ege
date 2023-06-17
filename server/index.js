78abdb196e098a664893792ff71cc7a1a7b5c179
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-06-17 10:36:53 +0500

    add options to cors

--- server/index.js
+++ server/index.js
@@ -12,7 +12,10 @@ const PORT = process.env.APP_PORT;
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+}));
 app.use(express.json());
 app.use(express.static('static'));
 app.use(fileUpload({}));
