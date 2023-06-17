c57f17dadf0ceaeba061c9a6d79936d0b7c914e5
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-06-17 11:01:12 +0500

    use context and check auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,29 @@
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Option from './components/Option';
 import Home from './Home';
 import CreateTask from './CreateTask';
 import './App.css';
+import Login from './pages/Login';
+import { observer } from 'mobx-react-lite';
 
 
 function App() {
+    const { store } = useContext(Context);
+
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            store.checkAuth()
+        }
+    }, [])
+
+
     return (
         <div className="app">
             <BrowserRouter>
                 <Routes>
                     <Route path='/' element={<Home />} exact />
+                    <Route path='/login' element={<Login />} exact />
                     <Route path='/create' element={<CreateTask />} exact />
                     <Route path='/subject/:id' element={<Option />} exact />
                 </Routes>
@@ -20,4 +33,4 @@ function App() {
 }
 
 
-export default App;
+export default observer(App);
