39ddc943af4cfdf3cbf8ded111b2d1a3c90474c2
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-06-17 10:37:54 +0500

    add store to global context

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'
+import Store from './store';
 
+
+const store = new Store();
+
+export const Context = createContext({
+    store,
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Context.Provider
+        value={{ store }}
+    >
+        <App />
+    </Context.Provider>
 );
