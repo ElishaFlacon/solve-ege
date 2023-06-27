c5e87c3dc1923a652a7e15b7098add5d663078cf
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-06-27 23:27:49 +0500

    add footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import Button from './UI/button/Button';
+
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <a href='https://github.com/ElishaFlacon/solve-ege'>
+                <Button>
+                    GitHub
+                </Button>
+            </a>
+        </footer>
+    )
+}
+
+
+export default Footer;
