31da6ba38f7bcade52bbd03171e7620dff26a3d5
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-06-28 02:36:55 +0500

    add button logout and add check auth

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,7 +1,7 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Input from './UI/input/Input';
 import Button from './UI/button/Button';
-import Context from '../index';
+import { Context } from '../index';
 import { observer } from 'mobx-react-lite';
 
 
@@ -10,9 +10,15 @@ function LoginForm() {
     const [password, setPassword] = useState('');
     const { store } = useContext(Context);
 
+    useEffect(() => {
+        store.checkAuth();
+    }, [])
+
+    console.log(store);
 
     return (
         <div>
+            {store.isAuth && <div>вы авторизованы под {store.user.email}</div>}
             <Input
                 value={email}
                 onChange={(event) => setEmail(event.target.value)}
@@ -25,15 +31,14 @@ function LoginForm() {
                 type="password"
                 placeholder='Пароль'
             />
-            <Button
-                onClick={() => store.login(email, password)}
-            >
+            <Button onClick={() => store.login(email, password)}>
                 Войти
             </Button>
-            <Button
-                onClick={() => store.login(email, password)}
-            >
+            <Button onClick={() => store.registration(email, password)}>
                 Регистрация
+            </Button>
+            <Button onClick={() => store.logout()}>
+                выйти
             </Button>
         </div>
     );
